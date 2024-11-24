b3415aa4de83ce20fdfb9fb0418a3d8b7f14e58c
rtcpolk <robert.t.polk@vanderbilt.edu>
2024-11-24 18:08:36 -0500

    Added motorDriver configuration comments

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -10,14 +10,14 @@
  * the physical components of the mechanism. It then runs the general control loop. The control
  * loop...
  *
- * To properly configure this program to run with each of the components in the eyeball
- * mechanism, read through the following sections and set the variables accordingly. These
- * sections set pin connections and other parameters to interface with the physical setup. The
- * variables are explained in the comments above each section. The sections are in the following
- * order:
+ * To configure this program to interface with each of the components in the eyeball mechanism,
+ * read through the following sections and set the variables accordingly. Each section configures
+ * a different component of the program and outlines configuration and program variables. Only edit
+ * the configuration variables. The sections are in the following order:
  *      Logging
  *      BLE Client
  *      Encoders
+ *      Motors
  */
 
 //================================================================================================//
@@ -25,11 +25,12 @@
 // #include the necessary header files - Do not edit
 #include <Arduino.h>
 #include <ArduinoLog.h>
-#include "controller/clientHandler.h"
+#include <array>
+#include "controller/clientHandlerold.h"
 #include "controller/encoderHandler.h"
 
 /*
- * Configure Logging
+ * Logging
  *
  * This section determines the level of logging within the program. The messages are written to
  * the Serial monitor. The following log levels are available:
@@ -45,20 +46,17 @@
  *
  * Set the baud rate for serial communication. This should match the value in the platformio.ini
  * file.
- * 
- * Variables:
- *   LOG_LEVEL - The level of messages to show. Uncomment the desired level
- *   BAUD_RATE - The baud rate for serial communication
  */
 
-//constexpr uint8_t LOG_LEVEL = LOG_LEVEL_SILENT;
-// constexpr uint8_t LOG_LEVEL = LOG_LEVEL_FATAL;
+// Configuration Variables
+// constexpr uint8_t LOG_LEVEL = LOG_LEVEL_SILENT;   // The level of messages to show. Uncomment
+// constexpr uint8_t LOG_LEVEL = LOG_LEVEL_FATAL;    // the desired level
 // constexpr uint8_t LOG_LEVEL = LOG_LEVEL_ERROR;
 // constexpr uint8_t LOG_LEVEL = LOG_LEVEL_WARNING;
-//constexpr uint8_t LOG_LEVEL = LOG_LEVEL_NOTICE;
-constexpr uint8_t LOG_LEVEL = LOG_LEVEL_TRACE;
-//constexpr uint8_t LOG_LEVEL = LOG_LEVEL_VERBOSE;
-constexpr uint32_t BAUD_RATE = 115200;
+// constexpr uint8_t LOG_LEVEL = LOG_LEVEL_NOTICE;
+// constexpr uint8_t LOG_LEVEL = LOG_LEVEL_TRACE;
+constexpr uint8_t LOG_LEVEL = LOG_LEVEL_VERBOSE;
+constexpr uint32_t BAUD_RATE = 115200;  // The baud rate for serial communication
 
 /*
  * Configure BLE Client
@@ -66,50 +64,70 @@ constexpr uint32_t BAUD_RATE = 115200;
  * This section configures the BLE Client by setting the UUIDs and device name. The UUIDs need to
  * match those set in server/server.cpp in order for the client to connect properly. New UUIDs
  * can be generated at https://www.uuidgenerator.net/
- *
- * In addition to the variables below, there are three class constants that control scanning
- * events. They do not need to be adjusted however they can be found in the clientHandler.h file if
- * further optimization is to be done.
- *
- * Variables:
- *   SERVICE_UUID - The UUID for the service the client should connect to
- *   IMU_CHARACTERISTIC_UUID - The UUID for the IMU characteristic
- *   DEVICE_NAME - The name of the device that the client is on
  */
 
-const std::string SERVICE_UUID = "da2aa210-e2ab-4d96-8d94-8536ec5a2728";
-const std::string IMU_CHARACTERISTIC_UUID = "72b9a4be-85fe-4cd5-ae42-f32414542c5a";
-const std::string DEVICE_NAME = ""; // It seems to not connect when !empty
+// Configuration Variables
+const std::string SERVICE_UUID =
+        "da2aa210-e2ab-4d96-8d94-8536ec5a2728"; // The UUID for the service the client should
+// connect to
+const std::string IMU_CHARACTERISTIC_UUID =
+        "72b9a4be-85fe-4cd5-ae42-f32414542c5a"; // The UUID for the IMU characteristic
+const std::string DEVICE_NAME = "Controller";   // The name of the device that the client is on
+
+// Program Variables
+TaskHandle_t clientLoopHandle = nullptr;    // Ptr to the client's FreeRTOS task
 
 /*
- * Configure Encoders
+ * Encoders
  *
- * This section configures the encoders that track the motor shafts' rotation. Set the following
- * pin variables to the corresponding GPIO pins on the ESP32. Do not edit the encoderPins variable
- *
- * Variables:
- *   #_ENCODER_PIN_A - The Channel A pin for the # encoder. Corresponds to the yellow wire
- *   #_ENCODER_PIN_B - The Channel B pin for the # encoder. Corresponds to the white wire
- *   encoderPins - An array that holds all the encoder pins. Do not edit
+ * This section configures the encoders attached to the Pololu 34:1 motor which tracks the
+ * shafts' rotation. Set the following pin variables to the corresponding GPIO pins on the ESP32.
+ * The yellow wires correspond to Channel A, and white wires Channel B.
  */
-constexpr uint8_t FIRST_ENCODER_PIN_A = 15;
-constexpr uint8_t FIRST_ENCODER_PIN_B = 16;
-constexpr uint8_t SECOND_ENCODER_PIN_A = 17;
-constexpr uint8_t SECOND_ENCODER_PIN_B = 18;
-constexpr uint8_t THIRD_ENCODER_PIN_A = 19;
-constexpr uint8_t THIRD_ENCODER_PIN_B = 20;
+
+// Configuration Variables
+constexpr uint8_t FIRST_ENCODER_PIN_A = 0; // The Channel A pin for the first encoder
+constexpr uint8_t FIRST_ENCODER_PIN_B = 0; // The Channel B pin for the first encoder
+constexpr uint8_t SECOND_ENCODER_PIN_A = 0;
+constexpr uint8_t SECOND_ENCODER_PIN_B = 0;
+constexpr uint8_t THIRD_ENCODER_PIN_A = 0;
+constexpr uint8_t THIRD_ENCODER_PIN_B = 0;
+
+// Program Variables
 constexpr std::array<std::array<uint8_t, 2>, 3> encoderPins = {FIRST_ENCODER_PIN_A,
                                                                FIRST_ENCODER_PIN_B,
                                                                SECOND_ENCODER_PIN_A,
                                                                SECOND_ENCODER_PIN_B,
                                                                THIRD_ENCODER_PIN_A,
                                                                THIRD_ENCODER_PIN_B};
+TaskHandle_t encoderLoopHandle = nullptr;   // Ptr to the encoder's FreeRTOS task
+
+/*
+ * Configure Motors
+ *
+ * This section configures the motors with the DRV8871 motor drivers. Set the following pin
+ * variables to the corresponding GPIO pins on the ESP32. Set the PWM frequency and resolution
+ */
+
+// Configuration Variables
+constexpr uint8_t FIRST_DRIVER_PWM_PIN = 0; // GPIO pin connected to IN1
+constexpr uint8_t FIRST_DRIVER_DIRECTION_PIN = 0; // GPIO pin connected to IN2
+constexpr uint8_t SECOND_DRIVER_PWM_PIN = 0;
+constexpr uint8_t SECOND_DRIVER_DIRECTION_PIN = 0;
+constexpr uint8_t THIRD_DRIVER_PWM_PIN = 0;
+constexpr uint8_t THIRD_DRIVER_DIRECTION_PIN = 0;
+constexpr uint32_t PWM_FREQUENCY = 20000;   // The frequency of the PWM signal
+constexpr uint8_t PWM_RESOLUTION = 8;   // The resolution of the PWM duty cycle in bits
+
+// Program Variables
+constexpr std::array<std::array<uint8_t, 2>, 3> motorPins = {FIRST_DRIVER_DIRECTION_PIN,
+                                                             FIRST_DRIVER_PWM_PIN,
+                                                             SECOND_DRIVER_DIRECTION_PIN,
+                                                             SECOND_DRIVER_PWM_PIN,
+                                                             THIRD_DRIVER_DIRECTION_PIN,
+                                                             THIRD_DRIVER_PWM_PIN};
 
 //================================================================================================//
-
-TaskHandle_t encoderLoopHandle = nullptr;
-TaskHandle_t clientLoopHandle = nullptr;
-int count;
 
 /**
  * Restart the ESP32
@@ -158,57 +176,32 @@ void setup() {
 
     // Create background task for the encoder
     BaseType_t encoderResult = xTaskCreate(encoderLoopTask, "EncoderHandler::Loop",
-                                    2048, nullptr, 1, &encoderLoopHandle);
+                                           2048, nullptr, 1, &encoderLoopHandle);
 
     if (encoderResult != pdPASS) {
         Log.errorln("Failed to create encoderLoopTask");
         restart();
     }
 
-    // Initialize the BLE Client
-    try {
-        count = 0; //todo update this with client when ready
-    } catch (const std::exception &ex) {
-        Log.errorln("Failed to initialize ClientHandler - %s", ex.what());
-        restart();
-    } catch (...) {
-        Log.errorln("Failed to initialize ClientHandler - Unknown Error");
-        restart();
-    }
-
-    // Create background task for the client
-    BaseType_t clientResult = xTaskCreate(clientLoopTask, "EncoderHandler::Loop",
-                                    2048, nullptr, 2, &encoderLoopHandle);
-
-    if (clientResult != pdPASS) {
-        Log.errorln("Failed to create clientLoopTask");
-        restart();
-    }
+//    // Initialize the BLE Client
+//    try {
+//        count = 0; //todo update this with client when ready
+//    } catch (const std::exception &ex) {
+//        Log.errorln("Failed to initialize ClientHandler - %s", ex.what());
+//        restart();
+//    } catch (...) {
+//        Log.errorln("Failed to initialize ClientHandler - Unknown Error");
+//        restart();
+//    }
+//
+//    // Create background task for the client
+//    BaseType_t clientResult = xTaskCreate(clientLoopTask, "EncoderHandler::Loop",
+//                                    2048, nullptr, 2, &encoderLoopHandle);
+//
+//    if (clientResult != pdPASS) {
+//        Log.errorln("Failed to create clientLoopTask");
+//        restart();
+//    }
 }
 
-void loop() {
-    if (count == 3) {
-        Log.infoln("Suspending encoder loop");
-        vTaskSuspend(encoderLoopHandle);
-    }
-
-    if (count == 6) {
-        Log.infoln("Resuming encoder loop");
-        vTaskResume(encoderLoopHandle);
-    }
-
-    if (count == 9) {
-        Log.infoln("Suspending encoder loop");
-        vTaskSuspend(encoderLoopHandle);
-    }
-
-    if (count == 12) {
-        Log.infoln("Resuming encoder loop");
-        vTaskResume(encoderLoopHandle);
-    }
-
-    Log.infoln("I'm looping");
-
-    ++count;
-    delay(1000);
-}
+void loop() {}
